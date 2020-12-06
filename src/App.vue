bc767954f04e69c8fba5171f95d5c6725a9b286c
codwerk <louis_widdy@live.fr>
2020-12-06 20:02:06 +0100

    renommer le component(navbar)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
     <header>
-      <Navbar/>
+      <navbar/>
     </header>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
+import navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    navbar
   }
 }
 </script>
